58af6f10806f90fe63d12eaff569b6e04c915d72
Prassanna Subiramaniyan <Prassanna_Subiramaniyan@comcast.com>
2021-02-15 00:26:39 +0530

    added schema for initial graphql query

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const express = require("express");
+import express from 'express';
+const { graphqlHTTP } = require('express-graphql');
+import schema from './schema';
 
 const app = express();
 
-app.get('/',(req,resp) => {
-
-    resp.send('First Express code. horray');
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing!');
 });
 
-app.listen(8080,
-    ()=> console.log('Running Server on port 8080'));
+const root = { hello: () => "Hi, I'm Manny" };
+
+app.use(`/graphql`, graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}))
+
+app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
---
+++ schema.js
@@ -0,0 +1,13 @@
+import {buildSchema} from 'graphql'
+ 
+
+const schema = buildSchema(`
+    
+        type Query{
+            hello:String
+        }
+
+`);
+
+export default schema;
+ 
